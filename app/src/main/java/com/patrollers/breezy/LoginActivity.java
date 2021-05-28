c53c61d1300aa45db91b62b5173fb67e145818ec
Sai Sukesh <sukesh.sai04@gmail.com>
2021-05-29 00:19:27 +0530

    Added LoginPage and fragments

---
+++ app/src/main/java/com/patrollers/breezy/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.patrollers.breezy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.google.android.material.tabs.TabLayout;
+import com.patrollers.breezy.adapters.LoginAdapter;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static ProgressBar login_progress;
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    float v = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        login_progress = findViewById(R.id.login_progress);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTranslationY(300);
+        tabLayout.setAlpha(v);
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
---
+++ app/src/main/java/com/patrollers/breezy/adapters/LoginAdapter.java
@@ -0,0 +1,38 @@
+package com.patrollers.breezy.adapters;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.patrollers.breezy.fragments.LoginFragment;
+import com.patrollers.breezy.fragments.SignupFragment;
+
+public class LoginAdapter extends FragmentPagerAdapter {
+
+    Context context;
+    int totalTabs;
+
+    public LoginAdapter(FragmentManager fm, Context context, int totalTabs) {
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new LoginFragment();
+            case 1:
+                return new SignupFragment();
+            default:
+                return null;
+        }
+    }
+}
