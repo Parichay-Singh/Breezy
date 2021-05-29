edff8fa9d8ef31fa6aeccf63685c0282167abb02
Sai Sukesh <sukesh.sai04@gmail.com>
2021-05-29 14:57:40 +0530

    Added Database and updated HomeFragment

---
+++ app/src/main/java/com/patrollers/breezy/database/DailyDao.java
@@ -0,0 +1,46 @@
+package com.patrollers.breezy.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.patrollers.breezy.models.DailyPoints;
+
+import java.util.List;
+
+@Dao
+public interface DailyDao {
+
+    @Insert
+    void insertDaily(DailyPoints points);
+
+    @Query("SELECT * FROM dailyPointDb")
+    List<DailyPoints> loadAllPoints();
+
+    @Query("DELETE FROM dailyPointDb")
+    void resetDailyDb();
+
+    @Query("SELECT COUNT(*) FROM dailyPointDb WHERE date = :inputDate")
+    int isDatePresent(String inputDate);
+
+    @Query("SELECT sleep FROM dailyPointDb ORDER BY id DESC LIMIT 1")
+    int getSleep();
+
+    @Query("SELECT mood FROM dailyPointDb ORDER BY id DESC LIMIT 1")
+    int getMoodPoints();
+
+    @Query("SELECT exercise FROM dailyPointDb ORDER BY id DESC LIMIT 1")
+    boolean getExercise();
+
+    @Query("SELECT meditation FROM dailyPointDb ORDER BY id DESC LIMIT 1")
+    boolean getMeditate();
+
+    @Query("SELECT isHydrated FROM dailyPointDb ORDER BY id DESC LIMIT 1")
+    int getHydrated();
+
+    @Query("UPDATE dailyPointDb SET exercise = :exer WHERE id =(SELECT MAX(id) FROM dailyPointDb)")
+    void updateExercise(boolean exer);
+
+    @Query("UPDATE dailyPointDb SET meditation = :med WHERE id =(SELECT MAX(id) FROM dailyPointDb)")
+    void updateMeditate(boolean med);
+}
---
+++ app/src/main/java/com/patrollers/breezy/models/DailyPoints.java
@@ -0,0 +1,83 @@
+package com.patrollers.breezy.models;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "dailyPointDb")
+public class DailyPoints {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private int sleep, mood;
+    private boolean isHydrated, exercise, meditation;
+    private String date;
+
+    public DailyPoints() {
+    }
+
+    @Ignore
+    public DailyPoints(String date, int sleep, int mood, boolean isHydrated) {
+        this.date = date;
+        this.sleep = sleep;
+        this.mood = mood;
+        this.isHydrated = isHydrated;
+    }
+
+    public int getSleep() {
+        return sleep;
+    }
+
+    public void setSleep(int sleep) {
+        this.sleep = sleep;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public boolean isHydrated() {
+        return isHydrated;
+    }
+
+    public void setHydrated(boolean hydrated) {
+        isHydrated = hydrated;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isExercise() {
+        return exercise;
+    }
+
+    public void setExercise(boolean exercise) {
+        this.exercise = exercise;
+    }
+
+    public boolean isMeditation() {
+        return meditation;
+    }
+
+    public void setMeditation(boolean meditation) {
+        this.meditation = meditation;
+    }
+}
