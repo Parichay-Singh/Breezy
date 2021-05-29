bc1bde5e05fc00bf142416f4cf9cb1ca71704a1d
Sai Sukesh <sukesh.sai04@gmail.com>
2021-05-29 11:38:10 +0530

    Completed Home & Profile and added Questionnaire

--- app/src/main/java/com/patrollers/breezy/fragments/HomeFragment.java
+++ app/src/main/java/com/patrollers/breezy/fragments/HomeFragment.java
@@ -1,23 +1,88 @@
 package com.patrollers.breezy.fragments;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.patrollers.breezy.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
+
+    @BindView(R.id.username) TextView username;
+    @BindView(R.id.detect_disease) TextView detect_disease;
+    @BindView(R.id.graphView) LineChart graphView;
 
     public HomeFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ButterKnife.bind(this, root);
+
+        SharedPreferences userPrefs = getContext().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        username.setText(userPrefs.getString("Name", "Name") + ",");
+        detect_disease.setText(userPrefs.getString("Disease", "None"));
+
+        graphView.setDragEnabled(true);
+        graphView.setScaleEnabled(true);
+        graphView.getAxisRight().setDrawGridLines(false);
+        graphView.getAxisLeft().setDrawGridLines(false);
+        graphView.getXAxis().setDrawGridLines(false);
+        graphView.getAxisLeft().setDrawLabels(false);
+        graphView.getAxisRight().setDrawLabels(false);
+        graphView.getXAxis().setDrawLabels(false);
+
+        List<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 70f));
+        yValues.add(new Entry(3, 30f));
+        yValues.add(new Entry(4, 50f));
+        yValues.add(new Entry(5, 60f));
+        yValues.add(new Entry(6, 65f));
+
+        LineDataSet set = new LineDataSet(yValues, "Mood Graph");
+        set.setFillAlpha(110);
+
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.fade_graph);
+        set.setDrawFilled(true);
+        set.setFillDrawable(drawable);
+        set.setColor(Color.rgb(0, 119, 182));
+        set.setLineWidth(1.5f);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set);
+
+        LineData data = new LineData(dataSets);
+
+        graphView.setData(data);
+
+        return root;
     }
 }
--- app/src/main/java/com/patrollers/breezy/fragments/ProfileFragment.java
+++ app/src/main/java/com/patrollers/breezy/fragments/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.patrollers.breezy.fragments;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +9,48 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.patrollers.breezy.LoginActivity;
 import com.patrollers.breezy.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class ProfileFragment extends Fragment {
+
+    @BindView(R.id.username_profile) TextView username_profile;
+    @BindView(R.id.profile_age) TextView profile_age;
+    @BindView(R.id.profile_contact) TextView profile_contact;
+    @BindView(R.id.profile_credit) TextView profile_credit;
+    @BindView(R.id.signout_btn) Button signout_btn;
 
     public ProfileFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ButterKnife.bind(this, root);
+
+        SharedPreferences userPrefs = getContext().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+
+        username_profile.setText(userPrefs.getString("Name", "Name"));
+        profile_age.setText(userPrefs.getString("Age", "100"));
+        profile_contact.setText(userPrefs.getString("Contact", "9876543210"));
+        profile_credit.setText(userPrefs.getString("Points", "0"));
+
+        signout_btn.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+            getActivity().finish();
+        });
+
+        return root;
     }
 }
